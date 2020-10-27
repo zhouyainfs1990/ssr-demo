c0aedcb74c4d54ba70dce5907c5bb3d177582a89
zhouyainfs1990 <zhouyainfs@163.com>
2020-10-27 14:44:02 +0800

    ssr express

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 // 第 1 步：创建一个 Vue 实例
 const Vue = require('vue')
-const app = new Vue({
-  template: `<div>Hello World</div>`
-})
-
+//创建服务器实例
+const server = require('express')()
 // 第 2 步：创建一个 renderer
 const renderer = require('vue-server-renderer').createRenderer()
 
-// 第 3 步：将 Vue 实例渲染为 HTML
-// app:被渲染的vue 实例
-// 回调函数，渲染结束后执行的回调
-// 回调函数的参数 err,错误对象
-// html:渲染成功的html 字符串
-renderer.renderToString(app, (err, html) => {
-  if (err) throw err
-  console.log(html)
-  // => <div data-server-rendered="true">Hello World</div>
+//服务器设置路由
+server.get('*', (req, res) => {
+
+    //创建vue 实例
+    const app = new Vue({
+        data: {
+            msg:"hello ssr"
+        },
+        template: `<div>访问的 URL 是： {{ msg }}</div>`
+    })
+
+    // 创建一个render
+
+    // html:被渲染成的字符串
+    renderer.renderToString(app, (err, html) => {
+        if (err) {
+            res.status(500).end('Internal Server Error')
+            return
+        }
+        res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset='utf-8'></meta>
+            <title >Hello</title>
+          </head>
+          <body>${html}</body>
+        </html>
+      `)
+    })
 })
 
-// 在 2.5.0+，如果没有传入回调函数，则会返回 Promise：
-// renderer.renderToString(app).then(html => {
-//   console.log(html)
-// }).catch(err => {
-//   console.error(err)
-// })
+server.listen(8080)
